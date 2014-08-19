e11c375364b0fa558aedf44657ea5b835f9bb5dd
李怀奇 <never.be.evil.debian@gmail.com>
2014-08-19 15:13:12 +0800

    自动补全组件 完成度30%

--- modules/mdAutoWord/md.autoWord.js
+++ modules/mdAutoWord/md.autoWord.js
@@ -4,28 +4,62 @@
 
 define(['jquery'], function (require, exports, module) {
     return (function ($) {
-        $.fn.autoWord = function () {
-            var el = $(this);
+
+        var MDAutoWord = function (options, el) {
+            this.CONFIG = $.extend({}, MDAutoWord.CONFIG, options);
+            this.init(el, this.CONFIG);
+
+        }
+
+        MDAutoWord.CONFIG = {
+            key: '@',
+            isTwins: false,
+            data: ['asdadz', 'qwe123123', 'zxzvzx', 'asdqwe', 'asdqweqw', 'zv312)(*&', '束手就擒和', '企鹅231请问怎么牛逼']
+        }
+
+        MDAutoWord.prototype.init = function (el, options) {
             var autoWorkSelector;
 
             $(document).on('keyup', el, function () {
-                var data = ['asdas', 'asdqweq', 'qweqwe', 'qwezasdas', 'hgfdsaqqqq', 'zxiquweuiwqr'];
-//                debugger;
+                debugger;
                 if (!autoWorkSelector) {
                     autoWorkSelector = $('<div class="md-auto-word-block"></div>')
-                        .html('<ol><li>' + data.join('</li><li>') + '</li></ol>')
+                        .html('<ol><li>' + options.data.join('</li><li>') + '</li></ol>')
                         .appendTo(el.parent());
-                }
 
+                    //注册li点击事件
+                    autoWorkSelector.on('click', autoWorkSelector.find('li'), function () {
+                        debugger;
+                        el.val(this.text);
+                    });
+
+                } else {
+                    MDAutoWord.show.call(autoWorkSelector);
+                }
             });
+
 
             el.focusout(function () {
                 if (autoWorkSelector) {
-                    autoWorkSelector.hide();
+                    MDAutoWord.hide.call(autoWorkSelector);
                 }
             });
 
+        };
 
+        MDAutoWord.prototype.hide = function () {
+            this.hide();
+            this.css('z-index', 0);
+        }
+
+        MDAutoWord.prototype.show = function () {
+            this.show();
+            this.css('z-index', 99);
+        }
+
+
+        $.fn.autoWord = function (options) {
+            return new MDAutoWord(options, $(this));
         }
 
 
