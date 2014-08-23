be0d5076c04005646c961dfa0bbe83a32650e7f1
李怀奇 <never.be.evil.debian@gmail.com>
2014-08-23 22:35:40 +0800

    自动完成，bug fix

--- modules/mdAutoWord/md.autoWord.js
+++ modules/mdAutoWord/md.autoWord.js
@@ -4,7 +4,7 @@
 
 define(['jquery'], function (require, exports, module) {
     return (function ($) {
-        var el, isVisible = false;
+        var el, elValue, isVisible = false;
 
         var MDAutoWord = function (options) {
             this.CONFIG = $.extend({}, MDAutoWord.CONFIG, options);
@@ -17,7 +17,7 @@ define(['jquery'], function (require, exports, module) {
             isTwins: false,
             listLength: 10,
             isLoopList: true,
-            data: ['asdadz', 'qwe123123', 'zxzvzx', 'asdqwe', 'asdqweqw', 'zv312)(*&', '束手就擒和', '企鹅231请问怎么牛逼']
+            data: ['asdadz', 'qwe123123', 'zxzvzx', 'asdqwe', 'asdqweqw', 'zva312)(*&', '束手就擒和', '企鹅231请问怎么牛逼'].sort()
         }
 
         MDAutoWord.WhichKey = {
@@ -42,9 +42,35 @@ define(['jquery'], function (require, exports, module) {
                         event.preventDefault();
                     }
                 },
+
                 'keyup': function () {
-                    var elVal = el.val();
-                    if (elVal.substr(elVal.length - 1) != options.key)return;
+                    var elValue = el.val();
+                    //当列表为展开&&不为快捷键&&最后字符不为关键符，根据最后一个关键字reload列表
+                    if (isVisible && !(event.which == MDAutoWord.WhichKey.UP
+                        || event.which == MDAutoWord.WhichKey.DOWN
+                        || event.which == MDAutoWord.WhichKey.LEFT
+                        || event.which == MDAutoWord.WhichKey.RIGHT
+                        || event.which == MDAutoWord.WhichKey.ESC
+                        || event.which == MDAutoWord.WhichKey.ENTER) && (lastChar = elValue.substr(elValue.length - 1)) != options.key) {
+                        if (!lastChar)return;
+                        var lastIndexOfKeyWord = elValue.substring(elValue.lastIndexOf(options.key) + 1, elValue.length);
+
+                        var firstRes , sencondsRes;
+                        firstRes = sencondsRes = '';
+                        for (var i = options.data.length - 1; i >= 0; i--) {
+                            var item = options.data[i];
+                            var positionIndex = item.indexOf(lastIndexOfKeyWord);
+                            if (positionIndex == 0) {
+                                firstRes += '<li>' + item.replace(lastIndexOfKeyWord, '<b>' + lastIndexOfKeyWord + '</b>') + '</li>';
+                            } else if (positionIndex != -1) {
+                                sencondsRes += '<li>' + item.replace(lastIndexOfKeyWord, '<b>' + lastIndexOfKeyWord + '</b>') + '</li>';
+                            }
+                        }
+
+                        autoWordSelector[0].innerHTML = ('<ol>' + firstRes + sencondsRes + '</ol>').replace('<li>', '<li class="hover">');
+                    }
+
+
                     if (!autoWordSelector) {
                         autoWordSelector = $('<div class="md-auto-word-block"></div>')
                             .html('<ol><li class="hover">' + options.data.join('</li><li>') + '</li></ol>')
@@ -68,37 +94,36 @@ define(['jquery'], function (require, exports, module) {
                         return;
                     }
 
+
                     //TODO::eq(0)===[0]
                     if (!isVisible) {
                         _this.show.call(autoWordSelector, autoWordLISelector.eq(0));
                     }
 
+                    var hoverSelector = $('.md-auto-word-block ol').find('.hover');
                     switch (event.which) {
                         case MDAutoWord.WhichKey.DOWN:
-                            var hoverSelector = autoWordSelector.find('li.hover');
                             if (hoverSelector.next().length) {
-                                autoWordSelector.find('li.hover').removeClass('hover').next().addClass('hover');
+                                hoverSelector.removeClass('hover').next().addClass('hover');
                             } else if (MDAutoWord.CONFIG.isLoopList) {
-                                autoWordSelector.find('li.hover').removeClass('hover');
-                                autoWordLISelector.eq(0).addClass('hover');
+                                hoverSelector.removeClass('hover').siblings().eq(0).addClass('hover');
                             }
                             break;
                         case MDAutoWord.WhichKey.UP:
-                            var hoverSelector = autoWordSelector.find('li.hover');
                             if (hoverSelector.prev().length) {
                                 hoverSelector.removeClass('hover').prev().addClass('hover');
                             } else if (MDAutoWord.CONFIG.isLoopList) {
-                                autoWordSelector.find('li.hover').removeClass('hover');
-                                autoWordLISelector.eq(-1).addClass('hover');
+                                hoverSelector.removeClass('hover').siblings().eq(-1).addClass('hover');
                             }
                             break;
                         case MDAutoWord.WhichKey.ESC:
                             _this.hide.call(autoWordSelector, autoWordLISelector);
                             break;
                         case MDAutoWord.WhichKey.ENTER:
-                            autoWordSelector.find('li.hover').trigger('click');
+                            hoverSelector.trigger('click');
                             break;
                     }
+
                 }
             });
         };
@@ -107,7 +132,7 @@ define(['jquery'], function (require, exports, module) {
             this.hide();
             this.css('z-index', 0);
             li.removeClass('hover');
-//            el.focus();
+            el.focus();
             isVisible = false;
         }
 
@@ -123,20 +148,21 @@ define(['jquery'], function (require, exports, module) {
             return new MDAutoWord(options);
         }
 
-        $.fn.getCursorPosition = function () {
-            var el = $(this).get(0);
-            var pos = 0;
-            if ('selectionStart' in el) {
-                pos = el.selectionStart;
-            } else if ('selection' in document) {
-                el.focus();
-                var Sel = document.selection.createRange();
-                var SelLength = document.selection.createRange().text.length;
-                Sel.moveStart('character', -el.value.length);
-                pos = Sel.text.length - SelLength;
-            }
-            return pos;
-        }
+//        $.fn.getCursorPosition = function () {
+//            var el = $(this).get(0);
+//
+//            var pos = 0;
+//            if ('selectionStart' in el) {
+//                pos = el.selectionStart;
+//            } else if ('selection' in document) {
+//                el.focus();
+//                var Sel = document.selection.createRange();
+//                var SelLength = document.selection.createRange().text.length;
+//                Sel.moveStart('character', -el.value.length);
+//                pos = Sel.text.length - SelLength;
+//            }
+//            return pos;
+//        }
 
 
     })(jQuery);
