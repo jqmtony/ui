2ce1cf16f433e321888cd271b52f7ab947487f0e
李怀奇 <never.be.evil.debian@gmail.com>
2014-10-16 18:54:58 +0800

    update placeholder
    fix at use val function not work

--- modules/mdPlaceholder/md.placeholder.js
+++ modules/mdPlaceholder/md.placeholder.js
@@ -5,20 +5,52 @@
 define(function (require, eports, module) {
     return (function ($) {
         if ('placeholder' in document.createElement('input') == false) {
+            $('head').append($('<style>.mdPlaceholder{color:#BFBFBF !important;}</style>'));
             $(document).on({
                 focusin: function () {
-                    $(this).val('').addClass('placeHolderColor');
+                    $(this)
+                        .removeClass('mdPlaceholder');
+                    origValFn.call($(this), function (index, value) {
+                        var $this = $(this), placeholder = $this.attr('placeholder');
+                        if (value != placeholder) {
+                            return value;
+                        }
+                        return '';
+                    });
                 },
                 focusout: function () {
-                    $(this).val($(this).attr('placeholder')).removeClass('placeholder');
+                    origValFn.call($(this), function (index, value) {
+                        var $this = $(this);
+                        //has not value
+                        if (!value.trim()) {
+                            return $this.addClass('mdPlaceholder').attr('placeholder');
+                        } else {
+                            $this.removeClass('mdPlaceholder');
+                        }
+                        return value;
+                    });
                 }
             }, '[placeholder]')
                 .find('[placeholder]')
-                .addClass('placeHolderColor')
+                .addClass('mdPlaceholder')
                 .val(function () {
                     return $(this).attr('placeholder');
                 });
+
+            var origValFn = $.fn.val;
+            $.fn.val = function () {
+                var returnValue = origValFn.apply(this, arguments);
+
+                //fix at use jquery val function change element value,the element focusout event not trigger.
+                var $this = $(this);
+                if ($this.attr('placeholder') && arguments.length != 0) {
+                    $this.trigger('focusout');
+                }
+
+                return returnValue;
+            };
+
+
         }
     })(jQuery);
-
 });
